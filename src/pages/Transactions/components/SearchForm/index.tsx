d63db4f7d11c907f8bebecd44c19315a81b93dee
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2024-08-02 17:34:07 -0300

    search form zod

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,29 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styled";
+import { useForm } from "react-hook-form";
+import * as zod from 'zod' 
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = zod.object({
+    query: zod.string(),
+})
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-    return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Buscar por transação" />
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    })
 
-            <button type="submit">
+    const handleSearchTransactions = (data: SearchFormInputs) => {
+        console.log(data)
+    }
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input type="text" placeholder="Buscar por transação" { ...register('query') } />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
                 Buscar
             </button>
--- src/pages/Transactions/components/SearchForm/styled.ts
+++ src/pages/Transactions/components/SearchForm/styled.ts
@@ -31,11 +31,16 @@ export const SearchFormContainer = styled.form`
         border-radius: 6px;
         cursor: pointer;
 
-        &:hover {
-            background: transparent;
-            border: 1px solid ${props => props.theme["green-300"]};
-            color: ${props => props.theme["green-300"]};
-            transition: background-color 2s, color 0.2s, border-color 0.2s;
+        &:disabled {
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
+
+        &:not(:disabled):hover {
+            background: ${props => props.theme["green-500"]};
+            border-color: ${props => props.theme["green-500"]};
+            color: ${props => props.theme.white};
+            transition: background-color .2s, color 0.2s, border-color 0.2s;
         }
     }
 `
